3df09db34e88079d2da55812f9afa001034be1d1
Dirk Krummacker <krummacker@users.noreply.github.com>
2022-02-12 08:25:57 +0100

    Add project files and initial code

---
+++ MemoryHog/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace MemoryHog
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> list = new();
+
+            do
+            {
+                string data = "current list size: " + list.Count;
+                if (list.Count % 1000000 == 0)
+                {
+                    Console.WriteLine(data);
+                }
+                list.Add(data);
+            }
+            while (true);
+        }
+    }
+}
